c9d49251f55db798a9d63453ecd7ac723e615936
meifakun <runner.mei@gmail.com>
2020-04-08 09:55:06 +0800

    update revel_ext

--- go.mod
+++ go.mod
@@ -6,5 +6,17 @@ require (
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.2.0
+	github.com/inconshreveable/log15 v0.0.0-20200109203555-b30bc20e4fd1 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/revel/config v0.21.0 // indirect
+	github.com/revel/log15 v2.11.20+incompatible // indirect
+	github.com/revel/pathtree v0.0.0-20140121041023-41257a1839e9 // indirect
+	github.com/revel/revel v0.21.0
 	github.com/runner-mei/errors v0.0.0-20200320092244-1cbe0d062008
+	github.com/twinj/uuid v1.0.0 // indirect
+	github.com/xeonx/timeago v1.0.0-rc4 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	gopkg.in/fsnotify/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/stack.v0 v0.0.0-20141108040640-9b43fcefddd0 // indirect
 )
--- revel_ext/main.go
+++ revel_ext/main.go
@@ -1,16 +1,36 @@
 package revel_ext
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 
 	"github.com/revel/revel"
 	"github.com/runner-mei/validation"
 )
 
-func ToRevelErrors(errList []validation.ValidationError) []revel.ValidationError {
-	results := make(revel.ValidationError, len(errList))
+func TryToRevelValidateErrors(v *revel.Validation, err error, prefix string) bool {
+	e, ok := err.(interface {
+		ToValidationErrors() []validation.ValidationError
+	})
+	if ok {
+		errList := e.ToValidationErrors()
+		for idx := range errList {
+			v.Errors = append(v.Errors, &revel.ValidationError{
+				Key:     prefix + errList[idx].Key,
+				Message: errList[idx].Message,
+			})
+		}
+		return true
+	}
+	return false
+}
+
+func ToRevelErrors(errList []validation.ValidationError, prefix string) []revel.ValidationError {
+	results := make([]revel.ValidationError, len(errList))
 	for idx := range errList {
-		results[idx] = errList[idx]
+		results[idx].Key = prefix + errList[idx].Key
+		results[idx].Message = errList[idx].Message
 	}
 	return results
 }
@@ -22,7 +42,7 @@ func ToValidation(v *revel.Validation) *validation.Validation {
 	}
 }
 
-func Validate(value interface{}, v *revel.Validation) bool {
+func Validate(value interface{}, v *revel.Validation, prefix string) bool {
 	v1, ok := value.(interface {
 		Validate(*validation.Validation) bool
 	})
@@ -30,7 +50,10 @@ func Validate(value interface{}, v *revel.Validation) bool {
 		iv := ToValidation(v)
 		if v1.Validate(iv) {
 			for idx := range iv.Errors {
-				v.Errors = append(v.Errors, iv.Errors[idx])
+				v.Errors = append(v.Errors, &revel.ValidationError{
+					Key:     prefix + iv.Errors[idx].Key,
+					Message: iv.Errors[idx].Message,
+				})
 			}
 			return true
 		}
@@ -41,7 +64,10 @@ func Validate(value interface{}, v *revel.Validation) bool {
 		iv := ToValidation(v)
 		if v2(iv) {
 			for idx := range iv.Errors {
-				v.Errors = append(v.Errors, iv.Errors[idx])
+				v.Errors = append(v.Errors, &revel.ValidationError{
+					Key:     prefix + iv.Errors[idx].Key,
+					Message: iv.Errors[idx].Message,
+				})
 			}
 			return true
 		}
@@ -58,6 +84,12 @@ func Validate(value interface{}, v *revel.Validation) bool {
 	if ok {
 		return v4(v)
 	}
-	v.Errors = append(v.Errors, revel.ValidationError{Message: fmt.Sprintf("revel_ext.Validate: %T", value)})
-	return validation.HasErrors()
+	rv := reflect.ValueOf(value)
+	if rv.CanAddr() {
+		return Validate(rv.Addr().Interface(), v, prefix)
+	} else if rv.Kind() != reflect.Ptr {
+		panic(errors.New("请用指针试试"))
+	}
+	v.Errors = append(v.Errors, &revel.ValidationError{Message: fmt.Sprintf("revel_ext.Validate: %T", value)})
+	return v.HasErrors()
 }
