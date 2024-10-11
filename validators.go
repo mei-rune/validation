1d4a01a215d7c7f42537d83f5c1974d5fc384f0c
mfk <mfk@hengwei.com.cn>
2024-10-11 18:07:34 +0800

    redirect boo/validation

--- validators.go
+++ validators.go
@@ -5,503 +5,123 @@
 package validation
 
 import (
-	"errors"
-	"fmt"
-	"html"
-	"net"
-	"net/url"
-	"reflect"
 	"regexp"
-	"strconv"
-	"strings"
-	"time"
-	"unicode/utf8"
+
+	boovalidation "github.com/boo-admin/boo/validation"
 )
 
 var emptyMessageArgs = []interface{}{}
 
-type Validator interface {
-	IsSatisfied(interface{}) bool
-	DefaultMessage() string
-	Message() (string, []interface{})
-}
+type Validator = boovalidation.Validator
 
-type Required struct{}
+type Required = boovalidation.Required
 
 func ValidRequired() Required {
-	return Required{}
+	return boovalidation.ValidRequired()
 }
 
-func (r Required) IsSatisfied(obj interface{}) bool {
-	if obj == nil {
-		return false
-	}
-	switch v := reflect.ValueOf(obj); v.Kind() {
-	case reflect.Array, reflect.Slice, reflect.Map, reflect.String, reflect.Chan:
-		if v.Len() == 0 {
-			return false
-		}
-	case reflect.Ptr:
-		return r.IsSatisfied(reflect.Indirect(v).Interface())
-	}
-	return !reflect.DeepEqual(obj, reflect.Zero(reflect.TypeOf(obj)).Interface())
-}
-
-func (r Required) DefaultMessage() string {
-	return fmt.Sprintln("Required")
-}
-
-func (r Required) Message() (string, []interface{}) {
-	return "validation.required", emptyMessageArgs
-}
-
-type Min struct {
-	Min float64
-}
+type Min = boovalidation.Min
 
 func ValidMin(min int) Min {
-	return ValidMinFloat(float64(min))
+	return boovalidation.ValidMin(min)
 }
 
 func ValidMinFloat(min float64) Min {
-	return Min{min}
+	return boovalidation.ValidMinFloat(min)
 }
 
-func (m Min) IsSatisfied(obj interface{}) bool {
-	var (
-		num float64
-		ok  bool
-	)
-	switch reflect.TypeOf(obj).Kind() {
-	case reflect.Float64:
-		num, ok = obj.(float64)
-	case reflect.Float32:
-		ok = true
-		num = float64(obj.(float32))
-	case reflect.Int:
-		ok = true
-		num = float64(obj.(int))
-	}
-
-	if ok {
-		return num >= m.Min
-	}
-	return false
-}
-
-func (m Min) DefaultMessage() string {
-	return fmt.Sprintln("Minimum is", m.Min)
-}
-
-func (m Min) Message() (string, []interface{}) {
-	return "validation.minimum", []interface{}{m.Min}
-}
-
-type Max struct {
-	Max float64
-}
+type Max = boovalidation.Max
 
 func ValidMax(max int) Max {
-	return ValidMaxFloat(float64(max))
+	return boovalidation.ValidMax(max)
 }
 
 func ValidMaxFloat(max float64) Max {
-	return Max{max}
-}
-
-func (m Max) IsSatisfied(obj interface{}) bool {
-	var (
-		num float64
-		ok  bool
-	)
-	switch reflect.TypeOf(obj).Kind() {
-	case reflect.Float64:
-		num, ok = obj.(float64)
-	case reflect.Float32:
-		ok = true
-		num = float64(obj.(float32))
-	case reflect.Int:
-		ok = true
-		num = float64(obj.(int))
-	}
-
-	if ok {
-		return num <= m.Max
-	}
-	return false
-}
-
-func (m Max) DefaultMessage() string {
-	return fmt.Sprintln("Maximum is", m.Max)
-}
-
-func (m Max) Message() (string, []interface{}) {
-	return "validation.maximum", []interface{}{m.Max}
+	return boovalidation.ValidMaxFloat(max)
 }
 
 // Range requires an integer to be within Min, Max inclusive.
-type Range struct {
-	Min
-	Max
-}
+type Range = boovalidation.Range
 
 func ValidRange(min, max int) Range {
-	return ValidRangeFloat(float64(min), float64(max))
+	return boovalidation.ValidRange(min, max)
 }
 
 func ValidRangeFloat(min, max float64) Range {
-	return Range{Min{min}, Max{max}}
-}
-
-func (r Range) IsSatisfied(obj interface{}) bool {
-	return r.Min.IsSatisfied(obj) && r.Max.IsSatisfied(obj)
-}
-
-func (r Range) DefaultMessage() string {
-	return fmt.Sprintln("Range is", r.Min.Min, "to", r.Max.Max)
-}
-
-func (r Range) Message() (string, []interface{}) {
-	return "validation.range", []interface{}{r.Min.Min, r.Max.Max}
+	return boovalidation.ValidRangeFloat(min, max)
 }
 
 // MinSize requires an array or string to be at least a given length.
-type MinSize struct {
-	Min int
-}
+type MinSize = boovalidation.MinSize
 
 func ValidMinSize(min int) MinSize {
-	return MinSize{min}
-}
-
-func (m MinSize) IsSatisfied(obj interface{}) bool {
-	if str, ok := obj.(string); ok {
-		return utf8.RuneCountInString(str) >= m.Min
-	}
-	v := reflect.ValueOf(obj)
-	if v.Kind() == reflect.Slice {
-		return v.Len() >= m.Min
-	}
-	return false
-}
-
-func (m MinSize) DefaultMessage() string {
-	return fmt.Sprintln("Minimum size is", m.Min)
-}
-
-func (m MinSize) Message() (string, []interface{}) {
-	return "validation.min_size", []interface{}{m.Min}
+	return boovalidation.ValidMinSize(min)
 }
 
 // MaxSize requires an array or string to be at most a given length.
-type MaxSize struct {
-	Max int
-}
+type MaxSize = boovalidation.MaxSize
 
 func ValidMaxSize(max int) MaxSize {
-	return MaxSize{max}
-}
-
-func (m MaxSize) IsSatisfied(obj interface{}) bool {
-	if str, ok := obj.(string); ok {
-		return utf8.RuneCountInString(str) <= m.Max
-	}
-	v := reflect.ValueOf(obj)
-	if v.Kind() == reflect.Slice {
-		return v.Len() <= m.Max
-	}
-	return false
-}
-
-func (m MaxSize) DefaultMessage() string {
-	return fmt.Sprintln("Maximum size is", m.Max)
-}
-
-func (m MaxSize) Message() (string, []interface{}) {
-	return "validation.max_size", []interface{}{m.Max}
+	return boovalidation.ValidMaxSize(max)
 }
 
 // Length requires an array or string to be exactly a given length.
-type Length struct {
-	N int
-}
+type Length = boovalidation.Length
 
 func ValidLength(n int) Length {
-	return Length{n}
-}
-
-func (s Length) IsSatisfied(obj interface{}) bool {
-	if str, ok := obj.(string); ok {
-		return utf8.RuneCountInString(str) == s.N
-	}
-	v := reflect.ValueOf(obj)
-	if v.Kind() == reflect.Slice {
-		return v.Len() == s.N
-	}
-	return false
-}
-
-func (s Length) DefaultMessage() string {
-	return fmt.Sprintln("Required length is", s.N)
-}
-
-func (s Length) Message() (string, []interface{}) {
-	return "validation.length", []interface{}{s.N}
+	return boovalidation.ValidLength(n)
 }
 
 // Match requires a string to match a given regex.
-type Match struct {
-	Regexp *regexp.Regexp
-}
+type Match = boovalidation.Match
 
 func ValidMatch(regex *regexp.Regexp) Match {
-	return Match{regex}
+	return boovalidation.ValidMatch(regex)
 }
 
-func (m Match) IsSatisfied(obj interface{}) bool {
-	str := obj.(string)
-	return m.Regexp.MatchString(str)
-}
-
-func (m Match) DefaultMessage() string {
-	return fmt.Sprintln("Must match", m.Regexp)
-}
-
-func (m Match) Message() (string, []interface{}) {
-	return "validation.match", []interface{}{m.Regexp.String()}
-}
-
-var emailPattern = regexp.MustCompile("^[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[a-zA-Z0-9](?:[\\w-]*[\\w])?$")
-
-type Email struct {
-	Match
-}
+type Email = boovalidation.Email
 
 func ValidEmail() Email {
-	return Email{Match{emailPattern}}
-}
-
-func (e Email) DefaultMessage() string {
-	return fmt.Sprintln("Must be a valid email address")
-}
-
-func (e Email) Message() (string, []interface{}) {
-	return "validation.email", emptyMessageArgs
+	return boovalidation.ValidEmail()
 }
 
 const (
-	None               = 0
-	IPAny              = 1
-	IPv4               = 32 // IPv4 (32 chars)
-	IPv6               = 39 // IPv6(39 chars)
-	IPv4MappedIPv6     = 45 // IP4-mapped IPv6 (45 chars) , Ex) ::FFFF:129.144.52.38
-	IPv4CIDR           = IPv4 + 3
-	IPv6CIDR           = IPv6 + 3
-	IPv4MappedIPv6CIDR = IPv4MappedIPv6 + 3
+	None               = boovalidation.None
+	IPAny              = boovalidation.IPAny
+	IPv4               = boovalidation.IPv4
+	IPv6               = boovalidation.IPv6
+	IPv4MappedIPv6     = boovalidation.IPv4MappedIPv6
+	IPv4CIDR           = boovalidation.IPv4CIDR
+	IPv6CIDR           = boovalidation.IPv6CIDR
+	IPv4MappedIPv6CIDR = boovalidation.IPv4MappedIPv6CIDR
 )
 
 // Requires a string(IP Address) to be within IP Pattern type inclusive.
-type IPAddr struct {
-	Vaildtypes []int
-}
+type IPAddr = boovalidation.IPAddr
 
 // Requires an IP Address string to be exactly a given  validation type (IPv4, IPv6, IPv4MappedIPv6, IPv4CIDR, IPv6CIDR, IPv4MappedIPv6CIDR OR IPAny)
 func ValidIPAddr(cktypes ...int) IPAddr {
-
-	for _, cktype := range cktypes {
-
-		if cktype != IPAny && cktype != IPv4 && cktype != IPv6 && cktype != IPv4MappedIPv6 && cktype != IPv4CIDR && cktype != IPv6CIDR && cktype != IPv4MappedIPv6CIDR {
-			return IPAddr{Vaildtypes: []int{None}}
-		}
-	}
-
-	return IPAddr{Vaildtypes: cktypes}
-}
-
-func isWithCIDR(str string, l int) bool {
-
-	if str[l-3] == '/' || str[l-2] == '/' {
-
-		cidr_bit := strings.Split(str, "/")
-		if 2 == len(cidr_bit) {
-			bit, err := strconv.Atoi(cidr_bit[1])
-			//IPv4 : 0~32, IPv6 : 0 ~ 128
-			if err == nil && bit >= 0 && bit <= 128 {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func getIPType(str string, l int) int {
-
-	if l < 3 { //least 3 chars (::F)
-		return None
-	}
-
-	has_dot := strings.Index(str[2:], ".")
-	has_colon := strings.Index(str[2:], ":")
-
-	switch {
-	case has_dot > -1 && has_colon == -1 && l >= 7 && l <= IPv4CIDR:
-		if isWithCIDR(str, l) == true {
-			return IPv4CIDR
-		} else {
-			return IPv4
-		}
-	case has_dot == -1 && has_colon > -1 && l >= 6 && l <= IPv6CIDR:
-		if isWithCIDR(str, l) == true {
-			return IPv6CIDR
-		} else {
-			return IPv6
-		}
-
-	case has_dot > -1 && has_colon > -1 && l >= 14 && l <= IPv4MappedIPv6:
-		if isWithCIDR(str, l) == true {
-			return IPv4MappedIPv6CIDR
-		} else {
-			return IPv4MappedIPv6
-		}
-	}
-
-	return None
-}
-
-func (i IPAddr) IsSatisfied(obj interface{}) bool {
-
-	if str, ok := obj.(string); ok {
-
-		l := len(str)
-		ret := getIPType(str, l)
-
-		for _, ck := range i.Vaildtypes {
-
-			if ret != None && (ck == ret || ck == IPAny) {
-
-				switch ret {
-				case IPv4, IPv6, IPv4MappedIPv6:
-					ip := net.ParseIP(str)
-
-					if ip != nil {
-						return true
-					}
-
-				case IPv4CIDR, IPv6CIDR, IPv4MappedIPv6CIDR:
-					_, _, err := net.ParseCIDR(str)
-					if err == nil {
-						return true
-					}
-				}
-			}
-		}
-	}
-
-	return false
-}
-
-func (i IPAddr) DefaultMessage() string {
-	return fmt.Sprintln("Must be a vaild IP address")
-}
-
-func (i IPAddr) Message() (string, []interface{}) {
-	return "validation.ipaddr", emptyMessageArgs
+	return boovalidation.ValidIPAddr(cktypes...)
 }
 
 // Requires a MAC Address string to be exactly
-type MacAddr struct{}
+type MacAddr = boovalidation.MacAddr
 
 func ValidMacAddr() MacAddr {
-
-	return MacAddr{}
+	return boovalidation.ValidMacAddr()
 }
-
-func (m MacAddr) IsSatisfied(obj interface{}) bool {
-
-	if str, ok := obj.(string); ok {
-		if _, err := net.ParseMAC(str); err == nil {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (m MacAddr) DefaultMessage() string {
-	return fmt.Sprintln("Must be a vaild MAC address")
-}
-
-func (m MacAddr) Message() (string, []interface{}) {
-	return "validation.macaddr", emptyMessageArgs
-}
-
-var domainPattern = regexp.MustCompile(`^(([a-zA-Z0-9-\p{L}]{1,63}\.)?(xn--)?[a-zA-Z0-9\p{L}]+(-[a-zA-Z0-9\p{L}]+)*\.)+[a-zA-Z\p{L}]{2,63}$`)
 
 // Requires a Domain string to be exactly
-type Domain struct {
-	Regexp *regexp.Regexp
-}
+type Domain = boovalidation.Domain
 
 func ValidDomain() Domain {
-	return Domain{domainPattern}
+	return boovalidation.ValidDomain()
 }
 
-func (d Domain) IsSatisfied(obj interface{}) bool {
-
-	if str, ok := obj.(string); ok {
-
-		l := len(str)
-		//can't exceed 253 chars.
-		if l > 253 {
-			return false
-		}
-
-		//first and last char must be alphanumeric
-		if str[l-1] == 46 || str[0] == 46 {
-			return false
-		}
-
-		return domainPattern.MatchString(str)
-	}
-
-	return false
-}
-
-func (d Domain) DefaultMessage() string {
-	return fmt.Sprintln("Must be a vaild domain address")
-}
-
-func (d Domain) Message() (string, []interface{}) {
-	return "validation.domain", emptyMessageArgs
-}
-
-var urlPattern = regexp.MustCompile(`^((((https?|ftps?|gopher|telnet|nntp)://)|(mailto:|news:))(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@#&=+$,A-Za-z0-9\p{L}])+)([).!';/?:,][[:blank:]])?$`)
-
-type URL struct {
-	Domain
-}
+type URL = boovalidation.URL
 
 func ValidURL() URL {
-	return URL{Domain: ValidDomain()}
-}
-
-func (u URL) IsSatisfied(obj interface{}) bool {
-	if str, ok := obj.(string); ok {
-
-		// TODO : Required lot of testing
-		return urlPattern.MatchString(str)
-	}
-
-	return false
-}
-
-func (u URL) DefaultMessage() string {
-	return fmt.Sprintln("Must be a vaild URL address")
-}
-
-func (u URL) Message() (string, []interface{}) {
-	return "validation.url", emptyMessageArgs
+	return boovalidation.ValidURL()
 }
 
 /*
@@ -509,209 +129,28 @@ NORMAL BenchmarkRegex-8   	2000000000	         0.24 ns/op
 STRICT BenchmarkLoop-8    	2000000000	         0.01 ns/op
 */
 const (
-	NORMAL = 0
-	STRICT = 4
+	NORMAL = boovalidation.NORMAL
+	STRICT = boovalidation.STRICT
 )
 
 // Requires a string to be without invisible characters
-type PureText struct {
-	Mode int
-}
+type PureText = boovalidation.PureText
 
 func ValidPureText(m int) PureText {
-	if m != NORMAL && m != STRICT { // Q:required fatal error
-		m = STRICT
-	}
-	return PureText{m}
-}
-
-func isPureTextStrict(str string) (bool, error) {
-
-	l := len(str)
-
-	for i := 0; i < l; i++ {
-
-		c := str[i]
-
-		// deny : control char (00-31 without 9(TAB) and Single 10(LF),13(CR)
-		if c >= 0 && c <= 31 && c != 9 && c != 10 && c != 13 {
-			return false, errors.New("detect control character")
-		}
-
-		// deny : control char (DEL)
-		if c == 127 {
-			return false, errors.New("detect control character (DEL)")
-		}
-
-		//deny : html tag (< ~ >)
-		if c == 60 {
-
-			ds := 0
-			for n := i; n < l; n++ {
-
-				// 60 (<) , 47(/) | 33(!) | 63(?)
-				if str[n] == 60 && n+1 <= l && (str[n+1] == 47 || str[n+1] == 33 || str[n+1] == 63) {
-					ds = 1
-					n += 3 //jump to next char
-				}
-
-				// 62 (>)
-				if ds == 1 && str[n] == 62 {
-					return false, errors.New("detect tag (<[!|?]~>)")
-				}
-			}
-		}
-
-		//deby : html encoded tag (&xxx;)
-		if c == 38 && i+1 <= l && str[i+1] != 35 {
-
-			max := i + 64
-			if max > l {
-				max = l
-			}
-			for n := i; n < max; n++ {
-				if str[n] == 59 {
-					return false, errors.New("detect html encoded ta (&XXX;)")
-				}
-			}
-		}
-	}
-
-	return true, nil
-}
-
-// Requires a string to match a given html tag elements regex pattern
-// referrer : http://www.w3schools.com/Tags/
-var elementPattern = regexp.MustCompile(`(?im)<(?P<tag>(/*\s*|\?*|\!*)(figcaption|expression|blockquote|plaintext|textarea|progress|optgroup|noscript|noframes|menuitem|frameset|fieldset|!DOCTYPE|datalist|colgroup|behavior|basefont|summary|section|isindex|details|caption|bgsound|article|address|acronym|strong|strike|source|select|script|output|option|object|legend|keygen|ilayer|iframe|header|footer|figure|dialog|center|canvas|button|applet|video|track|title|thead|tfoot|tbody|table|style|small|param|meter|layer|label|input|frame|embed|blink|audio|aside|alert|time|span|samp|ruby|meta|menu|mark|main|link|html|head|form|font|code|cite|body|base|area|abbr|xss|xml|wbr|var|svg|sup|sub|pre|nav|map|kbd|ins|img|div|dir|dfn|del|col|big|bdo|bdi|!--|ul|tt|tr|th|td|rt|rp|ol|li|hr|em|dt|dl|dd|br|u|s|q|p|i|b|a|(h[0-9]+)))([^><]*)([><]*)`)
-
-// Requires a string to match a given urlencoded regex pattern
-var urlencodedPattern = regexp.MustCompile(`(?im)(\%[0-9a-fA-F]{1,})`)
-
-// Requires a string to match a given control characters regex pattern (ASCII : 00-08, 11, 12, 14, 15-31)
-var controlcharPattern = regexp.MustCompile(`(?im)([\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+)`)
-
-func isPureTextNormal(str string) (bool, error) {
-
-	decoded_str := html.UnescapeString(str)
-
-	matched_urlencoded := urlencodedPattern.MatchString(decoded_str)
-	if matched_urlencoded == true {
-		temp_buf, err := url.QueryUnescape(decoded_str)
-		if err == nil {
-			decoded_str = temp_buf
-		}
-	}
-
-	matched_element := elementPattern.MatchString(decoded_str)
-	if matched_element == true {
-		return false, errors.New("detect html element")
-	}
-
-	matched_cc := controlcharPattern.MatchString(decoded_str)
-	if matched_cc == true {
-		return false, errors.New("detect control character")
-	}
-
-	return true, nil
-}
-
-func (p PureText) IsSatisfied(obj interface{}) bool {
-
-	if str, ok := obj.(string); ok {
-
-		var ret bool
-		switch p.Mode {
-		case STRICT:
-			ret, _ = isPureTextStrict(str)
-		case NORMAL:
-			ret, _ = isPureTextStrict(str)
-		}
-		return ret
-	}
-
-	return false
-}
-
-func (p PureText) DefaultMessage() string {
-	return fmt.Sprintln("Must be a vaild Text")
-}
-
-func (p PureText) Message() (string, []interface{}) {
-	return "validation.pure_text", emptyMessageArgs
+	return boovalidation.ValidPureText(m)
 }
 
 const (
-	ONLY_FILENAME       = 0
-	ALLOW_RELATIVE_PATH = 1
+	ONLY_FILENAME       = boovalidation.ONLY_FILENAME
+	ALLOW_RELATIVE_PATH = boovalidation.ALLOW_RELATIVE_PATH
 )
 
-const regexDenyFileNameCharList = `[\x00-\x1f|\x21-\x2c|\x3b-\x40|\x5b-\x5e|\x60|\x7b-\x7f]+`
-const regexDenyFileName = `|\x2e\x2e\x2f+`
-
-var checkAllowRelativePath = regexp.MustCompile(`(?m)(` + regexDenyFileNameCharList + `)`)
-var checkDenyRelativePath = regexp.MustCompile(`(?m)(` + regexDenyFileNameCharList + regexDenyFileName + `)`)
-
 // Requires an string to be sanitary file path
-type FilePath struct {
-	Mode int
-}
+type FilePath = boovalidation.FilePath
 
 func ValidFilePath(m int) FilePath {
-
-	if m != ONLY_FILENAME && m != ALLOW_RELATIVE_PATH {
-		m = ONLY_FILENAME
-	}
-	return FilePath{m}
-}
-
-func (f FilePath) IsSatisfied(obj interface{}) bool {
-
-	if str, ok := obj.(string); ok {
-
-		var ret bool
-		switch f.Mode {
-
-		case ALLOW_RELATIVE_PATH:
-			ret = checkAllowRelativePath.MatchString(str)
-			if ret == false {
-				return true
-			}
-		default: //ONLY_FILENAME
-			ret = checkDenyRelativePath.MatchString(str)
-			if ret == false {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func (f FilePath) DefaultMessage() string {
-	return fmt.Sprintln("Must be a unsanitary string")
-}
-
-func (f FilePath) Message() (string, []interface{}) {
-	return "validation.file_path", emptyMessageArgs
+	return boovalidation.ValidFilePath(m)
 }
 
 // 开始日期不能大于结束日期
-type TimeStartEndCheck struct {
-	start time.Time
-	end   time.Time
-}
-
-func (m TimeStartEndCheck) IsSatisfied(obj interface{}) bool {
-	if m.start.IsZero() || m.end.IsZero() {
-		return true
-	}
-	return !m.end.Before(m.start)
-}
-
-func (m TimeStartEndCheck) DefaultMessage() string {
-	return "start time must is less the end time"
-}
-
-func (m TimeStartEndCheck) Message() (string, []interface{}) {
-	return "validation.time_start_end_check", []interface{}{m.start, m.end}
-}
+type TimeStartEndCheck = boovalidation.TimeStartEndCheck
